d409b8fc88c34161d356ae9ac69493fddc012f1d
yoadster <yoadster@gmail.com>
2015-12-18 07:53:45 +0000

    Code base change to aid better artwork loading.

--- DDFNetwork.bundle/Contents/Code/__init__.py
+++ DDFNetwork.bundle/Contents/Code/__init__.py
@@ -9,6 +9,10 @@ from PIL import Image
 from cStringIO import StringIO
 
 VERSION_NO = '1.2013.06.02.1'
+DATEFORMAT = '%B %d, %Y'
+XPATHS = {
+        'MetadataDate': '//*[@id="sets_details"]/div[3]/div[1]/div[1]/div'
+    }
 
 def any(s):
     for v in s:
@@ -17,11 +21,18 @@ def any(s):
     return False
 
 def PerformSearch(keyword):
-    searchResults = HTML.ElementFromURL('http://ddfnetwork.com/tour-search-scene.php?freeword=' + urllib.quote(keyword))
+    if "http://" in keyword:
+        searchResults = HTML.ElementFromURL(keyword)
+    else:
+        searchResults = HTML.ElementFromURL('http://ddfnetwork.com/tour-search-scene.php?freeword=' + urllib.quote(keyword))
     return searchResults.xpath('//div[contains(@class,"scene")]')
 
 def Start():
     HTTP.CacheTime = CACHE_1DAY
+
+def SetDateMetadata(date):
+    date_object = datetime.strptime(date, DATEFORMAT)
+    return date_object
 
 class EXCAgent(Agent.Movies):
     name = 'DDF Network'
@@ -41,7 +52,7 @@ class EXCAgent(Agent.Movies):
         year = media.year
         if media.primary_metadata is not None:
             year = media.primary_metadata.year
-            
+
         searchResults = PerformSearch(title)
 
         for searchResult in searchResults[0].xpath('//p[contains(@class,"set_title")]//a'):
@@ -49,8 +60,7 @@ class EXCAgent(Agent.Movies):
             curID = searchResult.get('href').replace('/','_')
             score = 100 - Util.LevenshteinDistance(title.lower(), resultTitle.lower())
             results.Append(MetadataSearchResult(id = curID, name = resultTitle, score = score, lang = lang))
-                
-        results.Sort('score', descending=True)            
+            results.Sort('score', descending=True)            
 
     def update(self, metadata, media, lang):
 
@@ -62,15 +72,8 @@ class EXCAgent(Agent.Movies):
         metadata.title = detailsPageElements.xpath('//h1')[0].text_content()
         metadata.summary = detailsPageElements.xpath('//div[@id="sets_story"]')[0].text_content().replace('&13;', '').strip(' \t\n\r"') + "\n\n"
         metadata.tagline = detailsPageElements.xpath('/html/head/title')[0].text_content()
-
-        coverPage = HTML.ElementFromURL('http://ddfnetwork.com/tour-search-scene.php?freeword=' + urllib.quote(metadata.title))
-        cover = coverPage.xpath('//a[contains(@href,"' + url + '")]//img')[0].get('src')
-
-        metadata.posters[cover] = Proxy.Preview(HTTP.Request(cover).content, sort_order = 0)
-
-        #Background
-        backgroundUrl = detailsPageElements.xpath('//video[@id="html5_trailer_player"]')[0].get('data-preview')
-        metadata.art[backgroundUrl] = Proxy.Preview(HTTP.Request(backgroundUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = 0)
+        metadata.originally_available_at = SetDateMetadata(detailsPageElements.xpath(XPATHS['MetadataDate'])[0].text_content())
+        metadata.year = metadata.originally_available_at.year
 
         # Genres
         metadata.genres.clear()
@@ -118,3 +121,11 @@ class EXCAgent(Agent.Movies):
                 actor = actor.split(" aka ")[0]
             role.actor = actor
             metadata.collections.add(member.text_content().strip())
+
+        #Background
+        backgroundUrl = detailsPageElements.xpath('//video[@id="html5_trailer_player"]')[0].get('data-preview')
+        metadata.art[backgroundUrl] = Proxy.Preview(HTTP.Request(backgroundUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = 0)
+        coverPage = HTML.ElementFromURL('http://ddfnetwork.com/tour-search-scene.php?freeword=' + urllib.quote(metadata.title).replace('%5Cu2019','%E2%80%99'))
+        cover = coverPage.xpath('//a[contains(@href,"' + url + '")]//img')[0].get('src')
+
+        metadata.posters[cover] = Proxy.Preview(HTTP.Request(cover).content, sort_order = 0)
